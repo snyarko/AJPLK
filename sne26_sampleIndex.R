b83327e34f3dfd7be481dbb13efc5fb48286c52b
snyarko <snyarko@stuy.edu>
2016-10-02 22:28:15 -0400

    Add files via upload

---
+++ sne26_sampleIndex.R
@@ -0,0 +1,28 @@
+index <- read.csv("Google\ Drive/Intel/prmr_smpl_index.csv")
+#this file is a manual index of corresponding sample number and primer index number from my notebook.
+#the file's in the github repository.
+
+index$experiment <- "sne26"
+#I hardcoded this in, but if we're integrating with Darach's data it needs to be changed
+
+index$replicate <- substr(index$sample, 1,1)
+
+#indicating whether sample was upshifted based on number in sample name
+index$Q <- sub("1", T, index$Q)
+index$Q <- sub("2", T, index$Q)
+index$Q <- sub("3", T, index$Q)
+index$Q <- sub("4", F, index$Q)
+index$Q <- sub("5", F, index$Q)
+index$Q <- sub("6", F, index$Q)
+
+
+index$time <- substr(index$sample, 2,2)
+
+
+#indicating time via sample name
+index$time <- sub("A", "preShift", index$time)
+index$time <- sub("X", "preShift", index$time)
+index$time <- sub("B", "preShock", index$time)
+index$time <- sub("Y", "preShock", index$time)
+index$time <- sub("C", "postShock", index$time)
+index$time <- sub("Z", "postShock", index$time)
