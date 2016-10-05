3a7ea089d8245cfc9077dc09a31815590e3c4050
darachm <dchmiller@gmail.com>
2016-10-05 11:58:04 -0400

    tweaked the index generator, started tracking the final index

--- sne26_sampleIndex.R
+++ sne26_sampleIndex.R
@@ -1,4 +1,4 @@
-index <- read.csv("Google\ Drive/Intel/prmr_smpl_index.csv")
+index <- read.csv("prmr_smpl_index.csv")
 #this file is a manual index of corresponding sample number and primer index number from my notebook.
 #the file's in the github repository.
 
@@ -6,6 +6,7 @@ index$experiment <- "sne26"
 #I hardcoded this in, but if we're integrating with Darach's data it needs to be changed
 
 index$replicate <- substr(index$sample, 1,1)
+index$Q <- index$replicate
 
 #indicating whether sample was upshifted based on number in sample name
 index$Q <- sub("1", T, index$Q)
@@ -26,3 +27,6 @@ index$time <- sub("B", "preShock", index$time)
 index$time <- sub("Y", "preShock", index$time)
 index$time <- sub("C", "postShock", index$time)
 index$time <- sub("Z", "postShock", index$time)
+
+write.csv(index,file="sne26index.csv")
+
